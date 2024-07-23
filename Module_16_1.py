760acad2451518fcfa9a2bd2977ad94f191fe90d
Kerei1988 <162966037+Kerei1988@users.noreply.github.com>
2024-07-23 11:39:16 +0600

    Add files via upload

---
+++ Module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Path, HTTPException, Body
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome() -> dict:
+    return {"Welcome": 'Главная страница'}
+
+
+@app.get("/user/admin")
+async def admin() -> dict:
+    return {"admin": 'Вы вошли как администратор'}
+
+
+@app.get("/user/{user_id}")
+async def user_id(user_id: int) -> dict:
+    return {"user_id": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user")
+async def user(username: str, age: int) -> dict:
+    return {"user": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
